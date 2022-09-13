199b5c53b5d34cac37cf321a8ad3c0f880304890
isaacuhunmwangho <isaacco911@gmail.com>
2022-09-12 19:50:00 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "programmingis like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a place of art,\n");
+	return (0);
+}
