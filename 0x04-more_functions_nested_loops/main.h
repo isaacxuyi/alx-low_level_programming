f59f0e1ad809db9052c841e22fa7db4228312ab1
isaacuhunmwangho <isaacco911@gmail.com>
2022-09-15 20:19:30 -0700

    0th task

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+int _isupport(int c);
+int _isdigital int c);
+int mul(int a, int b);
+void print_number(void);
+void print_most_number(void);
+void more_numbers(void);
+void print_line(int n);
+void print_digonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
