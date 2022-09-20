712d5a0559812c31747f157a50fd1db5e5462cdf
isaacuhunmwangho <isaacco911@gmail.com>
2022-09-19 20:48:58 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,21 +7,21 @@
  */
 void puts_half(char *str)
 {
-	int len = 0;
+        int len = 0;
 
-	while (*str != '\0')
-	{
-		len++;
-		str++;
-	}
+        while (*str != '\0')
+        {
+                len++;
+                str++;
+        }
 
-	str -= (len /  2);
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
+        str -= (len /  2);
+        while (*str != '\0')
+        {
+                _putchar(*str);
+                str++;
+        }
 
-	_putchar('\n');
+        _putchar('\n');
 
 {
